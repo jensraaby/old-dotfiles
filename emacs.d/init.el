52d1cdf6fc4cd4c645b822d6b46785b72737132b
Jens Raaby <jens+github@m.raaby.co.uk>
2014-07-16 10:56:03 +0100

    added haskell to emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -76,6 +76,16 @@
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (add-hook 'python-mode-hook 'autopair-mode)
 
+; Haskell
+(require 'haskell-mode)
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 
 ;Whitespace
 (setq show-trailing-whitespace t)
